28dc4b72b82ad4c0f133b3aecc85d7d131192586
alexandroidii <Riccio.peacenlove@gmail.com>
2018-10-23 15:28:50 -0400

    Fragments not working but working on it.

--- app/src/main/java/ca/raiot/cst2335/raiot/MainActivity.java
+++ app/src/main/java/ca/raiot/cst2335/raiot/MainActivity.java
@@ -18,92 +18,51 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
-    private ListView deviceListView;
-    private ArrayList<String> deviceList;
-
+public class MainActivity extends AppCompatActivity  implements BottomNavigationView.OnNavigationItemSelectedListener  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.navigation);
 
-        bottomNavigationView.setOnNavigationItemSelectedListener
-                (new BottomNavigationView.OnNavigationItemSelectedListener() {
+        loadFragment(new DevicesFragment());
 
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Fragment selectFragment = null;
-                switch (item.getItemId()) {
-                    case R.id.navigation_devices:
-                        selectFragment = DevicesFragment.newInstance(1);
-                        break;
-                    case R.id.navigation_settings:
-                        mTextMessage.setText(R.string.title_settings);
-                        break;
-                    case R.id.navigation_help:
-                        mTextMessage.setText(R.string.title_help);
-                        break;
-                }
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                transaction.replace(R.id.frame_layout, selectFragment);
-                transaction.commit();
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+    }
 
-                return true;
-            }
-        });
 
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.frame_layout, DevicesFragment.newInstance(1));
-        transaction.commit();
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment selectFragment = null;
 
-        deviceListView = (ListView)findViewById(R.id.deviceList);
-        deviceList = new ArrayList<String>();
-        deviceList.add("Light 1");
-        deviceList.add("Light 2");
-        deviceList.add("Light 3");
-
-        DeviceAdapter deviceAdapter = new DeviceAdapter(this);
-        deviceListView.setAdapter(deviceAdapter);
+        switch (item.getItemId()) {
+            case R.id.navigation_devices:
+                selectFragment = new DevicesFragment();
+                break;
+            case R.id.navigation_settings:
+                selectFragment = new SettingsFragment();
+                break;
+            case R.id.navigation_help:
+                selectFragment = new HelpFragment();
+                break;
         }
 
-
-public class DeviceAdapter extends ArrayAdapter<String> {
-    public DeviceAdapter(Context context) {
-        super(context, 0);
+        return loadFragment(selectFragment);
     }
 
-    @Override
-    public int getCount() {
-        return deviceList.size();
-    }
-
-    @Override
-    public String getItem(int position) {
-        return deviceList.get(position);
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-
-        LayoutInflater inflater = MainActivity.this.getLayoutInflater();
-
-        View result = null;
-            result = inflater.inflate(R.layout.device_item_layout, null);
-
-
-        Switch deviceSwitch = (Switch) result.findViewById(R.id.deviceSwitch);
-        deviceSwitch.setText(getItem(position));
-
-        return result;
-
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.frame_layout, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 
 
-}
 
 }
