6d997308aeec7cdc0ced0f75fe2844f027256cb0
langenet <robert@lange.ca>
2018-11-03 13:07:13 -0400

    modified the JsonFragment to clean up passing urls
    Added change in calling Toast. Cannot be in doInBackgroung. Must in in onPreExecute
    
    Added Help fragment including scroll

--- app/src/main/java/ca/raiot/cst2335/raiot/HelpFragment.java
+++ app/src/main/java/ca/raiot/cst2335/raiot/HelpFragment.java
@@ -1,8 +1,7 @@
 package ca.raiot.cst2335.raiot;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -25,22 +24,22 @@ public class HelpFragment extends Fragment {
     //https://stackoverflow.com/questions/27092827/how-to-read-text-file-from-assets-in-fragment
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
        // View rootView = inflater.inflate(R.layout.fragment_help, container, false);
         return inflater.inflate(R.layout.fragment_help, container, false);
     }
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         String content = null;
         try {
 
-            InputStream is = getActivity().getAssets().open("HelpText.txt");
-            int size = is.available();
+            InputStream file = getActivity().getAssets().open("helptext.txt");
+            int size = file.available();
             byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
+            file.read(buffer);
+            file.close();
             content = new String(buffer,"UTF-8");
         } catch (IOException ex) {
             ex.printStackTrace();
@@ -48,31 +47,9 @@ public class HelpFragment extends Fragment {
         }
 
 
-        TextView output = getView().findViewById(R.id.helpFragment);
+        TextView output = getView().findViewById(R.id.helpHeader);
         output.setText(content);
 
     }
 
 }
-
-//
-//    @Override
-//    public void onViewCreated(Bundle savedInstanceState) {
-//        super.onActivityCreated(savedInstanceState);
-//
-//        try {
-//            InputStream is = getContext().getAssets().open("test.txt");
-//            int size = is.available();
-//            // Read the entire asset into a local byte buffer.
-//            byte[] buffer = new byte[size];
-//            is.read(buffer);
-//            is.close();
-//            content = new String(buffer,"UTF-8");
-//        } catch (IOException e) {
-//            // Should never happen!
-//            throw new RuntimeException(e);
-//        }
-//        TextView output= (TextView)getView().findViewById(R.id.fragment_help);
-//        output.setText(content);
-//    }
-//}
